a15928078c8d5a48003868b64909e77b6cb32a33
Karthik <1by17cs183@bmsit.in>
2023-04-21 17:52:35 -0500

    hueristic values added

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ webcam.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
     # webcam in image frames
 while True:
     _, frame = webcam.read()
+    hsv_frame= cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     height, width, _ = frame.shape
 
     cx=int(width/2)
@@ -28,8 +29,28 @@ while True:
     # HSV(hue-saturation-value)
     # color space
    # cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    pixel_center= frame[cy,cx]
-    print(pixel_center)
+    pixel_center= hsv_frame[cy,cx]
+
+    hue_value=pixel_center[0]
+    color="UNDEFINED"
+
+    if hue_value < 5:
+        color= "RED"
+    elif hue_value<22:
+        color="ORANGE"
+    elif hue_value< 33:
+        color="YELLOW"
+    elif hue_value<78:
+        color="GREEN"
+    elif hue_value<131:
+        color="BLUE"
+    elif hue_value<167:
+        color="VIOLET"
+    else:
+        color="RED"
+
+    pixel_center_bgr=hsv_frame[cy,cx]
+    cv2.putText(frame, color,(10,50), 0,1,(255,0,0),2)
     cv2.circle(frame, (cx,cy), 5, (255,0,0), 3)
     cv2.imshow('Frame',frame)
     key= cv2.waitKey(1)
