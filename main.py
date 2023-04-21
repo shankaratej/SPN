56cf2bfe8917d67b66ececd14733dedfa3fbdbe9
Karthik <1by17cs183@bmsit.in>
2023-04-21 17:24:16 -0500

    frames

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import cv2
 # Capturing video through webcam
 webcam = cv2.VideoCapture(0)
 webcam.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-webcam.set(cv2.CAP_PROP_FRAME_WIDTH, 720)
+webcam.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
 
 # Start a while loop
@@ -16,18 +16,22 @@ webcam.set(cv2.CAP_PROP_FRAME_WIDTH, 720)
     # Reading the video from the
     # webcam in image frames
 while True:
-    _, imageFrame = webcam.read()
-    heigth, width, _ = webcam.shape
+    _, frame = webcam.read()
+    height, width, _ = frame.shape
 
     cx=int(width/2)
-    cy=heigth/2
+    cy=int(height/2)
 
 
     # Convert the imageFrame in
     # BGR(RGB color space) to
     # HSV(hue-saturation-value)
     # color space
-    cv2.imshow("Frame", imageFrame)
+   # cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    pixel_center= frame[cy,cx]
+    print(pixel_center)
+    cv2.circle(frame, (cx,cy), 5, (255,0,0), 3)
+    cv2.imshow('Frame',frame)
     key= cv2.waitKey(1)
     if key == 27:
         break
